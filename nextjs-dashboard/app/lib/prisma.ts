33bc7106ec5039b004cecfc41238cc1cae40f994
Johnathan Babb <johnathanwbabb@gmail.com>
2026-01-25 20:03:51 -0700

    vercel fix

--- nextjs-dashboard/app/lib/prisma.ts
+++ nextjs-dashboard/app/lib/prisma.ts
@@ -4,11 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    // Use Prisma Accelerate URL for queries
-    accelerateUrl: process.env.PRISMA_DATABASE_URL,
-  });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
