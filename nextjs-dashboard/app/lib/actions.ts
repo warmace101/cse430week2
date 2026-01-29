989a469633c7957987dc747bb6dcb5b33eda757d
Johnathan Babb <johnathanwbabb@gmail.com>
2026-01-29 14:50:12 -0700

    vercel fix 2

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -120,9 +120,8 @@ export async function deleteInvoice(id: string) {
       where: { id },
     });
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    throw new Error('Database Error: Failed to Delete Invoice.');
   }
 }
 
